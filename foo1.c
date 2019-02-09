2e304a71f3f6c601aab0dac04c0d49df57d45a95
aclgn <ademcilginiu@gmail.com>
2019-02-10 00:46:09 +0300

    printf Function

--- foo1.c
+++ foo1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello World\n");
+  printf("Hello World \n");
+  printf("this is first line \nSecond Line\t space ");
   return 0;
 }
